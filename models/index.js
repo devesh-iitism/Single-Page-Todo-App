ae7a432b8a78b15deb99dec8f17b42cdbd7b9252
root <dcdev001@gmail.com>
2020-10-14 21:10:22 +0000

    first edit

---
+++ models/index.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/todo-api', {
+	useNewUrlParser: true,
+  	useUnifiedTopology: true
+});
+
+mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
